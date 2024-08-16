5d3456c414fedde296c7145faf3746bde28515ba
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-08-16 23:49:40 +0300

    small fix

--- src/Category.py
+++ src/Category.py
@@ -1,5 +1,10 @@
+from typing import Any
+
+from src.Product import Product
+
+
 class Category:
-    '''Класс для представления продукта'''
+    """Класс для представления продукта"""
 
     name: str
     description: str
@@ -8,7 +13,7 @@ class Category:
     product_count: int
 
     def __init__(self, name: str, description: str, products: list):
-        '''Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.'''
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
 
         self.name = name
         self.description = description
@@ -17,19 +22,16 @@ class Category:
         Category.product_count = len(products)
 
     @property
-    def products(self):
-        products_str = ''
+    def products(self) -> Any:
+        products_str = ""
         for product in self.__products:
-            products_str += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.'
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
         return products_str
 
-    def add_product(self, product):
+    def add_product(self, product: Product) -> Any:
         self.__products.append(product)
         Category.product_count += 1
 
     @property
-    def products_list(self):
+    def products_list(self) -> Any:
         return self.__products
-
-
-
--- src/Product.py
+++ src/Product.py
@@ -1,16 +1,16 @@
-from src.Category import Category
+from typing import Any
 
 
 class Product:
-    '''Класс для представления продукта'''
+    """Класс для представления продукта"""
 
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
-        '''Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.'''
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
 
         self.name = name
         self.description = description
@@ -18,23 +18,28 @@ class Product:
         self.quantity = quantity
 
     @classmethod
-    def new_product(cls, new_product: dict, products_list: list):
+    def new_product(cls, new_product: dict, products_list: list) -> Any:
+        '''Классметод для добавления нового продукта с проверкой на уже существующие продукты в списке продуктов'''
         for product in products_list:
-            if new_product['name'] == product.name:
-                product.quantity += new_product['quantity']
-                if new_product['price'] >= product.price:
-                    product.price = new_product['price']
+            if new_product["name"] == product.name:
+                product.quantity += new_product["quantity"]
+                if new_product["price"] >= product.price:
+                    product.price = new_product["price"]
                 return Product(product.name, product.description, product.price, product.quantity)
             else:
-                return cls(new_product['name'], new_product['description'], new_product['price'], new_product['quantity'])
+                return cls(
+                    new_product["name"], new_product["description"], new_product["price"], new_product["quantity"]
+                )
 
     @property
-    def price(self):
+    def price(self) -> Any:
+        '''Геттер для вывода цены'''
         return self.__price
 
     @price.setter
-    def price(self, new_price):
+    def price(self, new_price: float) -> Any:
+        '''Сеттер для установления новой цены с проверкой на нулевые и отрицательные значения'''
         if new_price <= 0:
-            print('Цена не должна быть нулевая или отрицательная')
+            print("Цена не должна быть нулевая или отрицательная")
         else:
             self.__price = new_price
